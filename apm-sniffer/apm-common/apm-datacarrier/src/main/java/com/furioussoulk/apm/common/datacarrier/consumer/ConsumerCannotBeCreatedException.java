ec3b2a73719338cc5174c49fe99245a84527e75a
szj <200765821@qq.com>
2019-06-07 15:41:01 +0800

    6.0.ga

--- apm-sniffer/apm-common/apm-datacarrier/src/main/java/com/furioussoulk/apm/common/datacarrier/consumer/ConsumerCannotBeCreatedException.java
+++
@@ -1,7 +0,0 @@
-package com.furioussoulk.apm.common.datacarrier.consumer;
-
-public class ConsumerCannotBeCreatedException extends RuntimeException {
-    ConsumerCannotBeCreatedException(Throwable t) {
-        super(t);
-    }
-}
